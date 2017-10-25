c7a23a29c997273be585b032cc0a6567c237b754
Casey Webb <notcaseywebb@gmail.com>
2017-10-24 21:46:50 -0500

    :tada: init :tada:

---
+++ README.md
@@ -0,0 +1,59 @@
+# taskr-rollup
+> [Rollup][] plugin for [Taskr][]
+
+[![NPM][npm-shield]][npm]
+[![License][license-shield]][license]
+[![Build Status][travis-ci-shield]][travis-ci]
+[![Coverage Status][codecov-shield]][codecov]
+[![Dependency Status][david-dm-shield]][david-dm]
+
+
+## Install
+
+This plugin requires [Taskr][] and [Rollup][].
+
+```bash
+$ yarn add -D taskr rollup taskr-rollup
+```
+*or*
+```bash
+$ npm install taskr rollup taskr-rollup
+```
+
+## Usage
+
+```js
+exports.bundle = function * (fly) {
+  yield fly
+    // bundle entry file
+    .source('src/entry.js')
+    .rollup({
+      // configuration options, https://rollupjs.org/#configuration-files
+      plugins: [
+        require('rollup-plugin-babel')()
+      ],
+      output: {
+        format: 'es'
+      }
+    })
+    .target('dist')
+}
+```
+
+[Taskr]: https://github.com/lukeed/taskr
+[Rollup]: https://github.com/rollup/rollup
+
+[npm]: https://npmjs.com/package/taskr-rollup
+[npm-shield]: https://img.shields.io/npm/v/taskr-rollup.svg
+
+[license]: ./LICENSE
+[license-shield]: https://img.shields.io/npm/l/taskr-rollup.svg
+
+[travis-ci]: https://travis-ci.org/caseyWebb/taskr-rollup/
+[travis-ci-shield]: https://img.shields.io/travis/caseyWebb/taskr-rollup/master.svg
+
+[codecov]: https://codecov.io/gh/caseyWebb/taskr-rollup
+[codecov-shield]: https://img.shields.io/codecov/c/github/caseyWebb/taskr-rollup.svg
+
+[david-dm]: https://david-dm.org/caseyWebb/taskr-rollup
+[david-dm-shield]: https://img.shields.io/david/caseyWebb/taskr-rollup.svg
---
+++ test/test.js
@@ -0,0 +1,97 @@
+'use strict'
+
+const path = require('path')
+const test = require('tape')
+const Taskr = require('taskr')
+
+const dir = path.join(__dirname, 'fixtures')
+const tmp = path.join(__dirname, 'tmp')
+
+const expected =
+`var module = (function () {
+'use strict';
+
+var a = function () {
+  return 'a';
+};
+
+var b = function () {
+  return 'b';
+};
+
+var entry = function () {
+  a();
+  b();
+};
+
+return entry;
+
+}());
+`
+
+const opts = {
+  plugins: [
+    require('rollup-plugin-babel')()
+  ],
+  treeshake: false,
+  output: {
+    name: 'module',
+    sourcemap: false,
+    format: 'iife'
+  }
+}
+
+test('taskr-rollup', t => {
+  t.plan(12)
+
+  const taskr = new Taskr({
+    plugins: [
+      require('../'),
+      require('@taskr/clear')
+    ],
+    tasks: {
+      * basic(f) {
+        yield f.source(`${dir}/entry.js`).rollup(opts).target(tmp)
+
+        const actual = yield f.$.read(`${tmp}/entry.js`, 'utf8')
+        t.ok(actual, 'writes output file')
+        t.equal(actual, expected, 'produces correct content')
+
+        yield f.clear(tmp)
+      },
+      * inline(f) {
+        opts.output.sourcemap = 'inline'
+        yield f.source(`${dir}/entry.js`).rollup(opts).target(tmp)
+
+        const actual = yield f.$.read(`${tmp}/entry.js`, 'utf8')
+        t.ok(actual, 'writes output file')
+        t.true(actual.indexOf(expected) > -1, 'produces correct content')
+        t.true(actual.indexOf('# sourceMappingURL=data:application/json;base64') > -1, 'appends `sourceMappingURL` content')
+
+        const base64Map = actual.split('base64')[1]
+        const utf8Map = Buffer.from(base64Map, 'base64')
+        t.doesNotThrow(JSON.parse.bind(JSON, utf8Map), 'base64 encodes inline sourcemap')
+        t.equals(3, JSON.parse(utf8Map).version, 'sourcemap has correct content')
+
+        yield f.clear(tmp)
+      },
+      * external(f) {
+        opts.output.sourcemap = true
+        yield f.source(`${dir}/entry.js`).rollup(opts).target(tmp)
+
+        const actual = yield f.$.read(`${tmp}/entry.js`, 'utf8')
+        const map = yield f.$.read(`${tmp}/entry.js.map`, 'utf8')
+        t.ok(actual, 'writes output file')
+        t.true(actual.indexOf(expected) > -1, 'produces correct content')
+        t.true(actual.indexOf('# sourceMappingURL=entry.js.map') > -1, 'appends `sourceMappingURL`')
+        t.true(map.indexOf('{"version":3') === 0, 'writes external sourcemap')
+
+        yield f.clear(tmp)
+      }
+    }
+  })
+
+  t.true('rollup' in taskr.plugins, 'attach `rollup()` plugin to taskr')
+
+  taskr.serial(['basic', 'inline', 'external'])
+})
